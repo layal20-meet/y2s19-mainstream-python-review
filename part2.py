eec2cfc5a01b6f1b8da420862e834f2d3e4b2709
myname21-meet <myname21@meet.mit.edu>
2019-07-28 11:16:59 -0400

    comment

--- part2.py
+++ part2.py
@@ -1,7 +1,18 @@
 # Part 2 of the Python Review lab.
 
 def encode(x, y):
-	pass
+	if 1<y<250 and 500<x<1000:
+		return(x*y)
+	else:
+		print("invalid input")
+def prime (a):
+	for i in range (2,a):
+		if a % i ==0:
+			return(False)
+	return(True)
 
 def decode(coded_message):
-	pass
+	for i in range(2,250):
+		x= coded_message//i
+		if 500<x<1000 and prime(x):
+			return(x,i)
